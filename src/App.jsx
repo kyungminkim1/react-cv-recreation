c243de65e8d2dc0cc91280b674bfc206d92145f7
kyungminkim1 <kyungminkimnz@gmail.com>
2024-12-13 15:44:40 +1300

    Fix App error

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,61 @@
 export default function App() {
-  <>
-    <div className="left-column">
-      <section>
-        <h1>KYUNG-MIN KIM</h1>
-        <span>Junior Web Developer</span>
-      </section>
+  return (
+    <>
+      <div className="left-column">
+        <section>
+          <h1>KYUNG-MIN KIM</h1>
+          <span>Junior Web Developer</span>
+        </section>
 
-      <section>
-        <h2>CONTACT</h2>
-        <span>0123456789</span>
-        <span>abc@email.com</span>
-        <span>New Zealand</span>
-      </section>
+        <section>
+          <h2>CONTACT</h2>
+          <span>0123456789</span>
+          <span>abc@email.com</span>
+          <span>New Zealand</span>
+        </section>
 
-      <section>
-        <h2>CONTACT</h2>
-      </section>
-      <section>
-        <h2>CAREER OBJECTIVE</h2>
-        <p>Aspiring graduate seeking to expand my learning under a reputable company.</p>
-      </section>
-      <section>
-        <h2>SKILLS</h2>
-      </section>
-    </div>
-    <div className="right-column">
-      <article>
-        <h2>EDUCATION</h2>
-        <div>
-          <p>
-            <b>University of Auckland</b>
-            <br />
-            Bachelor of Science
-            <br />
-            Major in Computer Science
-          </p>
-          <span>2016-2020</span>
-        </div>
-        <div>
-          <p>
-            <b>Unitec</b>
-            Graduate Diploma of Computer
-          </p>
-          <span>2022-2023</span>
-        </div>
-      </article>
-      <article>
-        <h2>PERSONAL PROJECTS</h2>
-      </article>
-      <article>
-        <h2>UNIVERSITY PROJECTS</h2>
-      </article>
-      <article>
-        <h2>WORK EXPERIENCE</h2>
-      </article>
-    </div>
-  </>
+        <section>
+          <h2>CONTACT</h2>
+        </section>
+        <section>
+          <h2>CAREER OBJECTIVE</h2>
+          <p>Aspiring graduate seeking to expand my learning under a reputable company.</p>
+        </section>
+        <section>
+          <h2>SKILLS</h2>
+        </section>
+      </div>
+      <div className="right-column">
+        <article>
+          <h2>EDUCATION</h2>
+          <div>
+            <p>
+              <b>University of Auckland</b>
+              <br />
+              Bachelor of Science
+              <br />
+              Major in Computer Science
+            </p>
+            <span>2016-2020</span>
+          </div>
+          <div>
+            <p>
+              <b>Unitec</b>
+              Graduate Diploma of Computer
+            </p>
+            <span>2022-2023</span>
+          </div>
+        </article>
+        <article>
+          <h2>PERSONAL PROJECTS</h2>
+        </article>
+        <article>
+          <h2>UNIVERSITY PROJECTS</h2>
+        </article>
+        <article>
+          <h2>WORK EXPERIENCE</h2>
+        </article>
+      </div>
+    </>
+  )
 }
